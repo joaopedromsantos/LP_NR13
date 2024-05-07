2e36cfca8b2c3dae7a0e4c3226bb892f8b42c657
joaopedromsantos <joaomartinscrf2019@gmail.com>
2024-05-07 20:19:53 -0300

    First Page Animation

---
+++ script.js
@@ -0,0 +1,34 @@
+function redirecionar() {
+    window.open("https://typebot.co/elite-nr13-bot", "_blank");
+  };
+
+  // Animações
+  ScrollReveal().reveal('#Curse-Name', {
+    origin: 'left',
+    duration: 3000,
+    distance: '40%'
+  });
+
+  ScrollReveal().reveal('#Curse-Title', {
+    origin: 'left',
+    duration: 4000, 
+    distance: '20%'
+  });
+
+  ScrollReveal().reveal('#h4_1', {
+    origin: 'left',
+    duration: 4000,
+    distance: '20%'
+  });
+
+  ScrollReveal().reveal('#logo_1', {
+    origin: 'top',
+    duration: 1000,
+    distance: '10%'
+  });
+
+  ScrollReveal().reveal('#button_1', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '10%'
+  });
