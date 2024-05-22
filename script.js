2c5ff981385eed37a4aab92707abb2fa87b3e52f
joaopedromsantos <joaomartinscrf2019@gmail.com>
2024-05-22 02:57:24 -0300

    Finalizado pagina de app

--- script.js
+++ script.js
@@ -5,81 +5,105 @@ function redirecionar() {
   // Animações
   ScrollReveal().reveal('#Curse-Name', {
     origin: 'left',
-    duration: 3000,
+    duration: 1300,
     distance: '40%'
   });
 
   ScrollReveal().reveal('#Curse-Title', {
     origin: 'left',
-    duration: 4000, 
+    duration: 1000, 
     distance: '20%'
   });
 
   ScrollReveal().reveal('#h4_1', {
     origin: 'left',
-    duration: 4000,
+    duration: 1600,
     distance: '20%'
   });
 
   ScrollReveal().reveal('#logo_1', {
     origin: 'top',
-    duration: 1000,
+    duration: 1900,
     distance: '10%'
   });
 
   ScrollReveal().reveal('#button_1', {
     origin: 'bottom',
-    duration: 1000,
+    duration: 2200,
     distance: '10%'
   });
 
   ScrollReveal().reveal('#card-0', {
     origin: 'left',
-    duration: 2500,
+    duration: 2000,
     distance: '20%'
   });
 
   ScrollReveal().reveal('#card-1', {
     origin: 'left',
-    duration: 2500,
+    duration: 2000,
     distance: '25%'
   });
 
   ScrollReveal().reveal('#card-2', {
     origin: 'left',
-    duration: 2500,
+    duration: 2000,
     distance: '30%'
   });
 
   ScrollReveal().reveal('#card-3', {
     origin: 'left',
-    duration: 2500,
+    duration: 2000,
     distance: '35%'
   });
 
   ScrollReveal().reveal('#card-4', {
     origin: 'left',
-    duration: 2500,
+    duration: 2000,
     distance: '40%'
   });
 
   ScrollReveal().reveal('#card-5', {
     origin: 'left',
-    duration: 2500,
+    duration: 2000,
     distance: '45%'
   });
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const buttons = document.querySelectorAll('.button-card');
-    
-    buttons.forEach(button => {
-      button.addEventListener('click', () => {
-        const card = button.closest('.card');
-        card.classList.toggle('is-flipped');
-      });
-    });
+  ScrollReveal().reveal('#title-app', {
+    origin: 'left',
+    duration: 1500,
+    distance: '30%'
   });
-  
+
+  ScrollReveal().reveal('#li-1', {
+    origin: 'left',
+    duration: 1800,
+    distance: '35%'
+  });
+
+  ScrollReveal().reveal('#li-2', {
+    origin: 'left',
+    duration: 2100,
+    distance: '40%'
+  });
+
+  ScrollReveal().reveal('#li-3', {
+    origin: 'left',
+    duration: 2400,
+    distance: '45%'
+  });
+
+  ScrollReveal().reveal('#button_2', {
+    origin: 'left',
+    duration: 2500,
+    distance: '50%'
+  });
+
+  ScrollReveal().reveal('#app_1', {
+    origin: 'left',
+    duration: 2500,
+    distance: '50%'
+  });
   
   
   
